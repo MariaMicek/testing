7b4a57b2b92767c828bf43f266c2918d4b159e06
Maria Micek <mmicek16@wp.pl>
2019-04-23 10:10:02 +0200

    different cases

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const add = (a, b) => a + b
 
-if (add(2, 7) !== 7) {
-    throw new Error ('błąd dodawania')
+if (add(2, 5) !== 7 || add(2, 10) !== 12 || add(0,0) !== 0) {
+    throw new Error ('błąd dodawania liczb całkowitych')
 }
 
-console.log('dodawanie działa')
+console.log('dodawanie liczb całkowitych działa')
+
+if (add('2', '5') !== 7) {
+    throw new Error ('błąd dodawania liczb w stringach')
+}
+
+console.log('dodawanie liczb w stringach działa')
+
+if (add(0.2, 0.4) !== 0.6) {
+    throw new Error ('błąd dodawania ułamków')
+}
+
+console.log('dodawanie ułamków działa')
+
