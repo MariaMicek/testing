1e8c0e04bb0aa4f85c122e618bf3b1b4b19bcfa2
Maria Micek <mmicek16@wp.pl>
2019-04-23 10:33:53 +0200

    code passed tests for different cases (adding numbers and strings)

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const add = (a, b) => a + b
+const add = (a, b) => {
+    const aToNumber = Number(a)
+    const bToNumber = Number(b)
+
+    if (aToNumber && bToNumber) {
+        return aToNumber + bToNumber
+    } else {
+        return 0
+    }
+}
+
 
 if (add(2, 5) !== 7 || add(2, 10) !== 12 || add(0,0) !== 0) {
     throw new Error ('błąd dodawania liczb całkowitych')
@@ -6,15 +16,20 @@ if (add(2, 5) !== 7 || add(2, 10) !== 12 || add(0,0) !== 0) {
 
 console.log('dodawanie liczb całkowitych działa')
 
-if (add('2', '5') !== 7) {
+if (add('2', '5') !== 7 || add('2', '10') !== 12) {
     throw new Error ('błąd dodawania liczb w stringach')
 }
 
 console.log('dodawanie liczb w stringach działa')
 
-if (add(0.2, 0.4) !== 0.6) {
-    throw new Error ('błąd dodawania ułamków')
+if (add('ala', 'ma kota') !== 0 || add('ala', 5) !== 0) {
+    throw new Error ('funkcja powinna zwracać zero dla stringów z literami')
 }
 
-console.log('dodawanie ułamków działa')
+console.log('funkcja zwraca zero dla stringów z literami')
 
+// if (add(0.2, 0.4) !== 0.6) {
+//     throw new Error ('błąd dodawania ułamków')
+// }
+
+// console.log('dodawanie ułamków działa')
